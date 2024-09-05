f88f7684c12d3dc53d4ac87a7fb24516dc1ddecf
Shay <154104485+ShayArgaman@users.noreply.github.com>
2024-09-05 23:30:11 +0300

    Update Plane.h

--- Plane.h
+++ Plane.h
@@ -20,11 +20,12 @@ typedef struct
 void			initPlane(Plane* pPlane, Plane* planeArr, int planeCount);
 ePlaneType		getPlaneType();
 const char*		GetPlaneTypeStr(int type); 
-int				getPlaneSN(Plane* planeArr, int planeCount);
+int			getPlaneSN(Plane* planeArr, int planeCount);
 Plane*			findPlaneBySN(Plane* planeArr, int count, int sn);
-int				isSerialNumUnique(Plane* planeArr, int planeCount, int num);
+int			isSerialNumUnique(Plane* planeArr, int planeCount, int num);
 void			printPlane(const Plane* pPlane);
 void			printPlaneV(void* val);
+void 			freePlane(void* pPlane);
 
 int			savePlaneToFile(const Plane* pPlane, FILE* fp);
 int			loadPlaneFromFile(Plane* pPlane, FILE* fp);
